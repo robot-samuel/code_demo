1d5d9b3813afff2d21414a865aeee7fa91259f3e
robot-samuel <cau_weiyuhu@163.com>
2018-08-05 00:31:23 -0700

    proto update

--- proto/protoall.sh
+++ proto/protoall.sh
@@ -3,13 +3,11 @@
 
 rm -fr ./cpp/*
 rm -fr ./c/*
-rm -fr ./Java/*
 rm -fr ./python/*
 
 PROJECT_NAME=robot2015_common
 
 protoc -I=. --cpp_out=./cpp/ *.proto
 protoc-c -I=. --c_out=./c/ *.proto
-protoc -I=. --java_out=./Java/ *.proto
 protoc -I=. --python_out=./python/ *.proto
 
